3dc8ce803fe6a93bc237ab5af9281f1ef2fc8ed3
Vadim <web.vadimkolosov@gmail.com>
2017-03-14 23:00:47 +0200

    in progress

--- _site/assets/js/custom.js
+++ _site/assets/js/custom.js
@@ -1,3 +1,38 @@
-var shape = document.querySelector('.logo svg path');
-var shapeLength = shape.getTotalLength();
-console.log(shapeLength);
+$(function() {
+  headlineAnimation();
+  hamburgerActive();
+  clickMainNav();
+});
+
+function headlineAnimation() {
+  $('.card--link-to-full').on('click', function() {
+  	$('.headline-one').addClass('animation-h2');
+  	$('.headline-two').addClass('animation-h3');
+  	$('.simple-line').addClass('card--block-effect-line');
+  });
+  $('.card--backbutton').on('click', function() {
+  	$('.headline-one').removeClass('animation-h2');
+  	$('.headline-two').removeClass('animation-h3');
+  	$('.simple-line').removeClass('card--block-effect-line');
+  });
+}
+
+function hamburgerActive() {
+  $('.nav-main--hamburger').on('click', function() {
+  	$('.nav-main--menu-wrapper').toggleClass('active-nav-menu');
+  	$('.nav-main--hamburger').toggleClass('active-hamburger-icon');
+    $('.headline-one').removeClass('animation-h2');
+    $('.headline-two').removeClass('animation-h3');
+    $('.simple-line').removeClass('card--block-effect-line');
+  });
+}
+
+function clickMainNav() {
+  $('.nav-main-link').on('click', function() {
+  	$('.nav-main--menu-wrapper').removeClass('active-nav-menu');
+  	$('.nav-main--hamburger').removeClass('active-hamburger-icon');
+    $('.headline-one').addClass('animation-h2');
+  	$('.headline-two').addClass('animation-h3');
+  	$('.simple-line').addClass('card--block-effect-line');
+  });
+}
